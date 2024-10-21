bc9dabd973d17e6188d27812d28ceb60ff696ed3
BarabashTV <164335010+BarabashTV@users.noreply.github.com>
2024-10-21 12:38:01 +0300

    Alkutila (notes) haetaan Axios-kirjaston avulla palvelimelta. Hoida datan hakeminen Effect hookilla.

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,17 @@
 import ReactDOM from "react-dom/client";
-
+import axios from "axios";
 import App from "./App";
 
-const notes = [
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+/*axios.get("http://localhost:3001/notes").then((response) => {
+  const notes = response.data;
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <App notes={notes} />
+  );
+});*/
+
+/*const notes = [
   {
     id: 1,
     content: "HTML is easy",
@@ -18,13 +27,21 @@ const notes = [
     content: "GET and POST are the most important methods of HTTP protocol",
     important: true,
   },
-];
+];*/
 
 /*const result = notes.map((note) => note.id);*/
 /*const result = notes.map((note) => note.content);*/
-const result = notes.map((note, i) => note.content);
-console.log(result);
+/*const result = notes.map((note, i) => note.content);
+console.log(result);*/
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+/*ReactDOM.createRoot(document.getElementById("root")).render(
   <App notes={notes} />
 );
+
+axios.get("http://localhost:3001/notes").then((response) => {
+  const notes = response.data;
+  console.log(notes);
+});
+
+const promise2 = axios.get("http://localhost:3001/foobar");
+console.log(promise2);*/
