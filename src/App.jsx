de9151050bd6e2826e8d737d191668605dcade30
BarabashTV <164335010+BarabashTV@users.noreply.github.com>
2024-10-22 00:10:03 +0300

    Palvelimen kanssa tapahtuvan kommunikoinnin eristäminen omaan moduuliin.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Note from "./components/Note";
+import noteService from "./services/Notes";
 
-const App = (props) => {
+const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    console.log("effect");
-    axios.get("http://localhost:3001/notes").then((response) => {
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
+    /*axios.get("http://localhost:3001/notes").then((response) => {
       console.log("promise fulfilled");
       setNotes(response.data);
-    });
+    });*/
   }, []);
   console.log("render", notes.length, "notes");
 
@@ -33,11 +36,30 @@ const App = (props) => {
       important: Math.random() > 0.5,
       id: String(notes.length + 1),
     };
+
+    /* axios.post("http://localhost:3001/notes", noteObject).then((response) => {
+      console.log(response);
+      setNotes(notes.concat(response.data));
+      setNewNote("");
+    });*/
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNote("");
+    });
+  };
+
+  /*const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+      id: String(notes.length + 1),
+    };
     console.log("button clicked", event.target);
 
     setNotes(notes.concat(noteObject));
     setNewNote("");
-  };
+  };*/
 
   const handleNoteChange = (event) => {
     console.log(event.target.value);
@@ -45,6 +67,19 @@ const App = (props) => {
   };
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
+  const toggleImportanceOf = (id) => {
+    /*const url = `http://localhost:3001/notes/${id}`;*/
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    /*axios.put(url, changedNote).then((response) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
+    });*/
+    noteService.update(id, changedNote).then((returnedNote) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+    });
+  };
 
   return (
     <div>
@@ -56,7 +91,11 @@ const App = (props) => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
